744d1daa055e32592eed19528493d356dd6e44a4
thentrsfs <filipstojkov54@gmail.com>
2026-02-28 17:20:26 +0100

    Fix for ad-section

--- app/components/sections/AdSection.tsx
+++ app/components/sections/AdSection.tsx
@@ -25,6 +25,7 @@ const AdSection = () => {
             start: "top top+=100px",
             end: "bottom bottom-=130px",
             pin: leftRef.current,
+              pinSpacing: false,
             scrub: 0.5
         })
       }) 
@@ -34,12 +35,12 @@ const AdSection = () => {
     }, {scope: sectionRef});
   return (
      <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen lg:grid grid-cols-2 flex flex-col-reverse max-md:gap-12 lg:px-24 px-6">
-          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform min-h-[300px]">
+          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform">
             <p className="lg:text-[100px] text-[38px] leading-10 lg:leading-24 tracking-tight max-w-3xl font-black uppercase font-plus">STUDIO/NORTH® helps <br className="max-md:hidden" /> brands <span className="dark:bg-rose bg-offwhite lg:w-18 lg:h-18 w-7 h-7 rounded-full inline-block"></span> connect w/ culture</p>
 <div className="uppercase font-medium ">Adweek <span className="font-bold">Agency Spotlight</span> </div>
             <CustomButton text="About Us" />
           </div>
-         <div className="bg-offwhite dark:bg-ink min-h-screen">
+         <div className="bg-offwhite dark:bg-ink">
   <video
     className="w-full h-full object-cover block"
     autoPlay
