c676d81d3b5b63ec8cb21b54a9a9ce7b0b27fc5b
thentrsfs <filipstojkov54@gmail.com>
2026-02-28 18:54:18 +0100

    fix for importing

--- app/components/sections/AdSection.tsx
+++ app/components/sections/AdSection.tsx
@@ -6,15 +6,17 @@ import { useGSAP } from "@gsap/react";
 import { useRef} from "react";
 import CustomButton from "../ui/CustomButton";
 
-gsap.registerPlugin(ScrollTrigger, useGSAP)
+gsap.registerPlugin(ScrollTrigger)
 
 const AdSection = () => {
 
-    const sectionRef = useRef<HTMLDivElement>(null);
-    const leftRef = useRef<HTMLDivElement>(null);
-
-    useGSAP(() => {
-      if(!sectionRef.current || !leftRef.current) return;
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const leftRef = useRef<HTMLDivElement>(null);
+  
+  useGSAP(() => {
+    const section = sectionRef.current;
+    const left = leftRef.current;
+    if(!section || !left) return;
       
 
       const mm = gsap.matchMedia();
@@ -35,7 +37,7 @@ const AdSection = () => {
     }, {scope: sectionRef});
   return (
      <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen h-full lg:grid grid-cols-2 max-md:flex flex-col-reverse max-md:gap-12 lg:px-24 px-6">
-          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform">
+          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform min-h-50">
             <p className="lg:text-[100px] text-[38px] leading-10 lg:leading-24 tracking-tight max-w-3xl font-black uppercase font-plus">STUDIO/NORTH® helps <br className="max-md:hidden" /> brands <span className="dark:bg-rose bg-offwhite lg:w-18 lg:h-18 w-7 h-7 rounded-full inline-block"></span> connect w/ culture</p>
 <div className="uppercase font-medium ">Adweek <span className="font-bold">Agency Spotlight</span> </div>
             <CustomButton text="About Us" />
