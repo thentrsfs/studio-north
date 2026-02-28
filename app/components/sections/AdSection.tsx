c5c49dafd758391bc410f951921cafa925dc807a
thentrsfs <filipstojkov54@gmail.com>
2026-02-28 19:03:59 +0100

    fix scroll trigger bugs

--- app/components/sections/AdSection.tsx
+++ app/components/sections/AdSection.tsx
@@ -6,7 +6,7 @@ import { useGSAP } from "@gsap/react";
 import { useRef} from "react";
 import CustomButton from "../ui/CustomButton";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, useGSAP)
 
 const AdSection = () => {
 
@@ -14,9 +14,7 @@ const AdSection = () => {
   const leftRef = useRef<HTMLDivElement>(null);
   
   useGSAP(() => {
-    const section = sectionRef.current;
-    const left = leftRef.current;
-    if(!section || !left) return;
+    if(!sectionRef.current || !leftRef.current) return;
       
 
       const mm = gsap.matchMedia();
@@ -27,7 +25,7 @@ const AdSection = () => {
             start: "top top+=100px",
             end: "bottom bottom-=130px",
             pin: leftRef.current,
-              pinSpacing: false,
+            pinSpacing: false,
             scrub: 0.5
         })
       }) 
@@ -37,10 +35,12 @@ const AdSection = () => {
     }, {scope: sectionRef});
   return (
      <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen h-full lg:grid grid-cols-2 max-md:flex flex-col-reverse max-md:gap-12 lg:px-24 px-6">
-          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform min-h-50">
+      <div ref={leftRef} >
+          <div className="flex flex-col lg:gap-4 gap-6 will-change-transform">
             <p className="lg:text-[100px] text-[38px] leading-10 lg:leading-24 tracking-tight max-w-3xl font-black uppercase font-plus">STUDIO/NORTH® helps <br className="max-md:hidden" /> brands <span className="dark:bg-rose bg-offwhite lg:w-18 lg:h-18 w-7 h-7 rounded-full inline-block"></span> connect w/ culture</p>
 <div className="uppercase font-medium ">Adweek <span className="font-bold">Agency Spotlight</span> </div>
             <CustomButton text="About Us" />
+            </div>
           </div>
          <div className="bg-offwhite dark:bg-ink">
   <video
