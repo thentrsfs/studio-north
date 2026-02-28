74b54b9b8b20eb7c4289ce214ba07911a28ffbef
thentrsfs <filipstojkov54@gmail.com>
2026-02-28 19:30:02 +0100

    fix

--- app/components/sections/AdSection.tsx
+++ app/components/sections/AdSection.tsx
@@ -13,39 +13,30 @@ const AdSection = () => {
   const sectionRef = useRef<HTMLDivElement>(null);
   const leftRef = useRef<HTMLDivElement>(null);
   
- useGSAP(() => {
-  if (!sectionRef.current || !leftRef.current) return;
+  useGSAP(() => {
+    if(!sectionRef.current || !leftRef.current) return;
+      
 
-  const ctx = gsap.context(() => {
-    const mm = gsap.matchMedia();
+      const mm = gsap.matchMedia();
+     // Desktop pinning only
+      mm.add("(min-width: 768px)", () => { 
+        ScrollTrigger.create({
+            trigger: sectionRef.current,
+            start: "top top+=100px",
+            end: "bottom bottom-=130px",
+            pin: leftRef.current,
+            pinSpacing: false,
+            scrub: 0.5
+        })
+      }) 
 
-    mm.add("(min-width: 768px)", () => {
-      const timeout = setTimeout(() => {
-        const trigger = ScrollTrigger.create({
-          trigger: sectionRef.current,
-          start: "top top+=100",
-          end: "bottom bottom-=130",
-          pin: leftRef.current,
-          pinSpacing: false,
-          scrub: 0.5,
-        });
-
-        ScrollTrigger.refresh();
-
-        return () => trigger.kill();
-      }, 100); // wait for layout + video
-
-      return () => clearTimeout(timeout);
-    });
-
-  }, sectionRef);
-
-  return () => ctx.revert();
-}, []);
+      return () => mm.revert();
+        
+    }, {scope: sectionRef});
   return (
-     <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen h-full max-md:flex flex-col-reverse  lg:grid grid-cols-2max-md:gap-12 lg:px-24 px-6">
-      <div ref={leftRef} >
-          <div className="lg:gap-4 gap-6 will-change-transform">
+     <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen h-full lg:grid grid-cols-2 max-md:gap-12 lg:px-24 px-6">
+      <div ref={leftRef}>
+          <div className="flex flex-col lg:gap-4 gap-6 will-change-transform">
             <p className="lg:text-[100px] text-[38px] leading-10 lg:leading-24 tracking-tight max-w-3xl font-black uppercase font-plus">STUDIO/NORTH® helps <br className="max-md:hidden" /> brands <span className="dark:bg-rose bg-offwhite lg:w-18 lg:h-18 w-7 h-7 rounded-full inline-block"></span> connect w/ culture</p>
 <div className="uppercase font-medium ">Adweek <span className="font-bold">Agency Spotlight</span> </div>
             <CustomButton text="About Us" />
