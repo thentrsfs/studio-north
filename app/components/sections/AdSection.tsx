d3b8f2b88ccd8c4643f95ae2e4867d7deb4db9d5
thentrsfs <filipstojkov54@gmail.com>
2026-02-28 19:33:06 +0100

    fix

--- app/components/sections/AdSection.tsx
+++ app/components/sections/AdSection.tsx
@@ -5,6 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { useRef} from "react";
 import CustomButton from "../ui/CustomButton";
+import { div } from "motion/react-client";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP)
 
@@ -34,9 +35,10 @@ const AdSection = () => {
         
     }, {scope: sectionRef});
   return (
-     <div ref={sectionRef} id="ad-section" className="relative lg:pt-50 pt-16 min-h-screen h-full lg:grid grid-cols-2 max-md:gap-12 lg:px-24 px-6">
-      <div ref={leftRef}>
-          <div className="flex flex-col lg:gap-4 gap-6 will-change-transform">
+    <section id="ad-section" className="relative lg:pt-50 pt-16">
+     <div ref={sectionRef} className="lg:pt-50 pt-16 lg:grid grid-cols-2 max-md:gap-12 lg:px-24 px-6">
+      <div>
+          <div ref={leftRef} className="flex flex-col lg:gap-4 gap-6 will-change-transform">
             <p className="lg:text-[100px] text-[38px] leading-10 lg:leading-24 tracking-tight max-w-3xl font-black uppercase font-plus">STUDIO/NORTH® helps <br className="max-md:hidden" /> brands <span className="dark:bg-rose bg-offwhite lg:w-18 lg:h-18 w-7 h-7 rounded-full inline-block"></span> connect w/ culture</p>
 <div className="uppercase font-medium ">Adweek <span className="font-bold">Agency Spotlight</span> </div>
             <CustomButton text="About Us" />
@@ -56,6 +58,7 @@ const AdSection = () => {
 </div>
 
         </div>
+        </section>
   )
 }
 
